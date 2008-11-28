40fc3f77ee91ba1efb979afb0bbfc2498f6caf04
Nathan Sutton <nate@sevenwire.com>
2008-11-27 22:54:00 -0600

    Moving rails requires to inside forgery so it will work as a gem in rails.

--- init.rb
+++ init.rb
@@ -1,7 +1,1 @@
 require File.dirname(__FILE__) + '/lib/forgery'
-
-if defined?(RAILS_ROOT)
-  Dir[File.expand_path(RAILS_ROOT + '/lib/forgery/**/*.rb')].uniq.each do |file|
-    require file
-  end
-end
--- lib/forgery.rb
+++ lib/forgery.rb
@@ -80,3 +80,9 @@ end
 Dir[File.expand_path(File.dirname(__FILE__) + '/forgeries/**/*.rb')].uniq.each do |file|
   require file
 end
+
+if defined?(RAILS_ROOT)
+  Dir[File.expand_path(RAILS_ROOT + '/lib/forgery/**/*.rb')].uniq.each do |file|
+    require file
+  end
+end
