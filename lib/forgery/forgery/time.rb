01f4c5f6568d942bc52b8d440f0d11f7e52acac8
Thomas <thomas@icdesign.com.au>
2010-01-08 17:02:21 +1100

    Renamed Time class to TimeZone due to conflicts in Forgery(:basic) class

--- lib/forgery/forgery/time.rb
+++ lib/forgery/forgery/time.rb
@@ -1,12 +1,12 @@
 # Generates random timezone information.
-class Forgery::Time < Forgery
+class Forgery::TimeZone < Forgery
 
   # Gets a random timezone out of the 'timezones' dictionary
   #
   #   Forgery(:timezone).timezone
   #   # => "Sydney"
-  def self.time_zone
-    dictionaries[:time_zones].random
+  def self.zone
+    dictionaries[:zones].random
   end
 
 end
--- spec/forgery/time_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe Forgery::Time do
-  it "should return a random timezone" do
-    Forgery.dictionaries[:time_zones].should include(Forgery::Time.time_zone)
-  end
-end
