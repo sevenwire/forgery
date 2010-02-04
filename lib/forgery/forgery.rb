3fd23b6c7f9f242e47adab69b941d01fb5673eac
lukas <lukas.westermann@gmail.com>
2010-02-04 12:43:46 +0100

    added custom load behaviour for forgery, so that load paths can be customized

--- lib/forgery/forgery.rb
+++ lib/forgery/forgery.rb
@@ -7,7 +7,16 @@ class Forgery
   def self.formats
     @@formats ||= Formats.new
   end
-
+  
+  def self.load_paths
+    @@load_paths ||= [File.dirname(__FILE__)]
+  end
+  
+  def self.load_from!(path)
+    self.load_paths << path
+    Dir[File.expand_path("#{path}/**/*.rb")].uniq.each { |file| require file }
+  end
+  
   def self.rails_root
     if defined?(RAILS_ROOT)
       RAILS_ROOT
