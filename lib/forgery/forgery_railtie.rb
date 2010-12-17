2783d9c36b325fdab51a61fed641089d20086d42
Mike Hickman <mhickman84@gmail.com>
2010-12-17 01:36:19 -0500

    Moved write path from rails initializer to rake task

--- lib/forgery/forgery_railtie.rb
+++ lib/forgery/forgery_railtie.rb
@@ -4,7 +4,6 @@ require 'rails'
 class ForgeryRailtie < Rails::Railtie
   initializer "forgery_railtie.initialize" do
     Forgery.load_from! "#{Rails.root}/lib/forgery"
-    Forgery::FileWriter.write_to! "#{Rails.root}/lib/forgery/dictionaries"
   end
 
   rake_tasks do
@@ -13,7 +12,8 @@ class ForgeryRailtie < Rails::Railtie
         dictionary_name = args[:dictionary_name].to_s || raise("parameter :dictionary_name is required")
         source_url = args[:source_url].to_s || raise("parameter :source_url is required")
         css_or_xpath = args[:css_or_xpath].to_s || raise("parameter :css_or_xpath is required")
-
+        
+        Forgery::FileWriter.write_to! "#{Rails.root}/lib/forgery/dictionaries"
         Forgery::FileWriter.create_dictionary dictionary_name, source_url, css_or_xpath
       end
     end
