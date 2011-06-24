24f126c74055bedd4dbe939a16006245abf59928
ClintKrollwood <simon.krollpfeifer@peritor.com>
2011-06-24 11:48:02 +0200

    That is not my day

--- lib/forgery/forgery/name.rb
+++ lib/forgery/forgery/name.rb
@@ -55,6 +55,6 @@ class Forgery::Name < Forgery
 	def self.degree
 		degree = dictionaries[:academic_degrees].random
 		post_degree = degree.eql?("Master") || degree.eql?("Bachelor")
-		"#{post_degree ? degree + " of" : degree} #{dictionaries[:fields_of_study]}"
+		"#{post_degree ? degree + " of" : degree} #{dictionaries[:fields_of_study].random}"
 	end
 end
