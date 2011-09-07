1542eb62189a9d1d6c927dca5fb4a1486801a4e6
Todd Mazierski <toddmazierski@gmail.com>
2011-09-07 13:54:09 -0400

    Added comments.

--- lib/forgery/forgery/credit_card.rb
+++ lib/forgery/forgery/credit_card.rb
@@ -1,3 +1,4 @@
+# Generates random credit card numbers.
 class Forgery::CreditCard < Forgery
 
   CARDS = [
@@ -7,10 +8,29 @@ class Forgery::CreditCard < Forgery
     {:type => 'Discover', :length => 16, :prefixes => ["6011"]}
   ]
 
+  # Gets a random credit card type
+  #
+  #   Forgery(:credit_card).type
+  #   # => "Visa"
   def self.type
     CARDS.choice[:type]
   end
 
+  # Gets a random credit card number
+  #
+  #   Forgery(:credit_card).number
+  #   # => "4539750423451972"
+  #
+  #   Forgery(:credit_card).number(:type => 'Visa', :length => 13)
+  #   # => "4556180133982"
+  #
+  # Supported Options
+  # [:type]
+  #   The credit card type. Defaults to a random selection.
+  # [:length]
+  #   The length of the credit card number. Defaults to the length for the selected card type.
+  # [:prefixes]
+  #   The allowed prefixes for the card number. Defaults to prefixes for the selected card type.
   def self.number(options={})
     # find a card by type specified, or select a card randomly
     card = if options[:type]
