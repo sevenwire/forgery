ba7255a3e944975e17f0467cd42a97c765643f6b
Adam Meehan <adam.meehan@gmail.com>
2012-06-19 13:01:00 +1000

    Cleanup forgery.rb, require extensions explicitly

--- lib/forgery.rb
+++ lib/forgery.rb
@@ -1,6 +1,3 @@
-# Require forgeries at the bottom of the file so Forgery works as a gem both
-# within rails and outside of it.
-
 module Forgery
   def self.[](forgery)
     tools.find(forgery)
@@ -27,12 +24,6 @@ module Forgery
     end
   end
 
-  def self.load_extensions
-    Dir[gem_path + 'forgery/extensions/**/*.rb'].each do |file|
-      require file
-    end
-  end
-
   def self.rails?
     !rails_root.nil?
   end
@@ -41,20 +32,8 @@ module Forgery
     @@gem_path ||= File.expand_path(File.dirname(__FILE__)) + '/'
   end
 
-end
-
-# Loading forgery helpers.
-require 'forgery/tool_resolver'
-require 'forgery/file_reader'
-require 'forgery/storage'
-require 'forgery/tool'
-require 'forgery/extend'
-require 'forgery/forgery_api'
-require 'forgery/version'
-
-# Deprecate Forgery classes using Forgery as base class by virtue
-# of calling dictionaries or formats methods on it.
-module Forgery
+  # Deprecate Forgery classes using Forgery as base class by virtue
+  # of calling dictionaries or formats methods on it.
   def self.dictionaries
     warn 'The Forgery base class is deprecated. Use Forgery::Tool as the base class for your forgery classes or tools.'
     Forgery::Tool.dictionaries
@@ -66,16 +45,26 @@ module Forgery
   end
 end
 
-Forgery.load_extensions
+require 'forgery/version'
+require 'forgery/tool_resolver'
+require 'forgery/file_reader'
+require 'forgery/storage'
+require 'forgery/tool'
+require 'forgery/extend'
+require 'forgery/forgery_api'
+require 'forgery/extensions/array'
+require 'forgery/extensions/range'
+require 'forgery/extensions/string'
 
-# Loading the forgery tools
 Dir[Forgery.gem_path + 'forgery/tools/**/*.rb'].each do |file|
   require file
 end
 
 # Loading rails forgeries to override current forgery methods and add new forgeries
-# Only run this for Rails < 3.0 since we need to use a Railtie to initialize >= 3.0
-Forgery.load_from! "#{Forgery.rails_root}/lib/forgery" if Forgery.rails? && Rails::VERSION::STRING < "3.0.0"
-
-# Include our Railtie if Rails >= 3.0.0
-require 'forgery/railtie' if Forgery.rails? && Rails::VERSION::STRING >= "3.0.0"
+if Forgery.rails?
+  if Rails::VERSION::STRING >= "3.0.0"
+    require 'forgery/railtie'
+  else
+    Forgery.load_from! "#{Forgery.rails_root}/lib/forgery"
+  end
+end
