bdcc5d3a1b9015345e5f05b8755df87949dd5697
masciugo <masciugo@gmail.com>
2013-04-09 12:03:58 +0200

    avoiding white spaces in domain_name and email_address

--- lib/forgery/forgery/internet.rb
+++ lib/forgery/forgery/internet.rb
@@ -1,7 +1,7 @@
 class Forgery::Internet < Forgery
 
   def self.user_name
-    (dictionaries[:male_first_names].random[0,1] + dictionaries[:last_names].random).downcase
+    (dictionaries[:male_first_names].random[0,1] + dictionaries[:last_names].random).downcase.strip.gsub(' ','_')
   end
 
   def self.top_level_domain
@@ -9,7 +9,7 @@ class Forgery::Internet < Forgery
   end
 
   def self.domain_name
-    dictionaries[:company_names].random.downcase + '.' + self.top_level_domain
+    dictionaries[:company_names].random.downcase.strip.gsub(' ','_') + '.' + self.top_level_domain
   end
 
   def self.email_address
