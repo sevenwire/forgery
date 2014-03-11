e3592fe32b82abade6c61aeea72ab0ee6b1563fd
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2014-03-11 11:07:23 +0100

    We still support Ruby 1.9.2

--- lib/forgery/forgery/russian_tax.rb
+++ lib/forgery/forgery/russian_tax.rb
@@ -6,12 +6,12 @@ class Forgery::RussianTax < Forgery
 
   #bank identification
   def self.bik
-    "04#{5.times.map { rand(1..9) }.join}#{rand(50..99)}"
+    "04#{5.times.map { rand(9) + 1 }.join}#{rand(50) + 50}"
   end
 
   #bank account format pretty simple
   def self.account_number
-    20.times.map { rand(1..9) }.join
+    20.times.map { rand(9) + 1 }.join
   end
 
   #taxation id
@@ -29,7 +29,7 @@ class Forgery::RussianTax < Forgery
   private
   def self.person_inn
     mask = [[7, 2, 4, 10, 3, 5, 9, 4, 6, 8], [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]]
-    inn = 12.times.map { rand(1..9)}.join
+    inn = 12.times.map { rand(9) + 1 }.join
     inn[10] = ((0..(inn.length-3)).inject(0) {|crc, i| crc + inn[i].to_i*mask[0][i].to_i} % 11 % 10).to_s
     inn[11] = ((0..(inn.length-2)).inject(0) {|crc, i| crc + inn[i].to_i*mask[1][i].to_i} % 11 % 10).to_s
     inn
@@ -37,19 +37,19 @@ class Forgery::RussianTax < Forgery
 
   def self.legal_inn
     mask = [2, 4, 10, 3, 5, 9, 4, 6, 8]
-    inn =  10.times.map { rand(1..9)}.join
+    inn =  10.times.map { rand(9) + 1 }.join
     inn[9] = ((0..(inn.length-2)).inject(0) {|crc, i| crc + inn[i].to_i*mask[i].to_i} % 11 % 10).to_s
     inn
   end
 
   def self.person_ogrn
-    ogrn = 14.times.map { rand(1..9) }.join
+    ogrn = 14.times.map { rand(9) + 1 }.join
     ogrn += (ogrn.to_i%13%10).to_s
     ogrn
   end
 
   def self.legal_ogrn
-    ogrn = 12.times.map { rand(1..9) }.join
+    ogrn = 12.times.map { rand(9) + 1 }.join
     ogrn += (ogrn.to_i%11%10).to_s
     ogrn
   end
