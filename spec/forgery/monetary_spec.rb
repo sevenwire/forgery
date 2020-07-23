c183fccb520ca939fb4232a0c92f967436fa0978
Brandon Arbini <b@arbini.dev>
2020-07-23 00:45:45 -0700

    Fix broken test on Ruby 2

--- spec/forgery/monetary_spec.rb
+++ spec/forgery/monetary_spec.rb
@@ -1,14 +1,14 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'bigdecimal'
 
 describe Forgery::Monetary do
-
-  it "should return random number string" do
+  it 'should return random number string' do
     expect(Forgery(:monetary).money).to match(/^[\d+\.]+$/)
   end
 
-  it "should return random number respecting min and max parameters" do
-    expect(BigDecimal.new(Forgery(:monetary).money({:min => 10, :max => 20}))).to be_between(10, 20)
+  it 'should return random number respecting min and max parameters' do
+    expect(BigDecimal(Forgery(:monetary).money({ min: 10, max: 20 }))).to be_between(10, 20)
   end
-
 end
